c306db5fa4cd94c710e9045cda9a0800351c85b2
GuGolF <s6552c10031@sau.ac.th>
2025-10-26 17:07:56 +0700

    update app by firebase

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <>
+      <div className="flex flex-col items-center mt-15">
+        <span className="text-lg font-bold text-gray-700">
+            Created by GuGolF DTI-SAU
+        </span>
+        <span className="text-lg font-bold text-gray-700">
+            Copyright © 2025 Southeast Asia University
+        </span>
+      </div>
+    </>
+  );
+}
